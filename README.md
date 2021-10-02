7aa51d18ebf08ae201744e197d8326f6b236c400
Owen <qcgzxw@qq.com>
2021-10-02 21:37:13 +0800

    多页合并

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ## pdf转图片
-指定转换pdf文件的某页保存为图片文件
+指定转换pdf文件的某几页保存为图片文件
 
 
 ### 用法:
 ```shell
 # exe文件可直接运行
-.\dist\pdf转图片.exe --input ../test.pdf --output ../test.png --page 1
+.\dist\pdf转图片.exe --input ../test.pdf --output ../test.png --start 1 --end 5
 
 # py脚本运行需要指定poppler目录
-python.exe .\pdf转图片.py --input test.pdf --output test.png --page 1
+python.exe .\pdf转图片.py --input test.pdf --output test.png --start 1 --end 5
 ```
 
 ### 打包:
--- pdf转图片.py
+++ pdf转图片.py
@@ -3,6 +3,7 @@ import getopt
 import os
 import sys
 from pdf2image import convert_from_path
+from PIL import Image
 from pdf2image.exceptions import (
     PDFInfoNotInstalledError,
     PDFPageCountError,
@@ -15,16 +16,36 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
-def pdf_to_jpeg(pdf, jpeg, index):
+def image_merge(image1, image2):
+    w1, h1 = image1.size
+    w2, h2 = image2.size
+    img = Image.new('RGB', (max(w1, w2), h1 + h2), 'white')
+    img.paste(image1)
+    img.paste(image2, (0, h1))
+    return img
+
+
+def pdf_to_jpeg(pdf, jpeg, start, end):
     if not os.path.exists(pdf):
         print('输入文件不存在！')
         sys.exit(2)
     try:
         images = convert_from_path(
             pdf_path=pdf,
-            poppler_path=resource_path(os.path.join("poppler-21.09.0", "Library", "bin"))
+            poppler_path=resource_path(os.path.join("poppler-21.09.0", "Library", "bin")),
+            first_page=start,
+            last_page=end,
         )
-        images[index].save(jpeg)
+        if len(images) == 1:
+            images[0].save(jpeg)
+        elif len(images) > 1:
+            first_image = images.pop(0)
+            for image in images:
+                first_image = image_merge(first_image, image)
+            first_image.save(jpeg)
+        else:
+            print('pdf文件页数错误')
+            sys.exit(2)
 
     except PDFInfoNotInstalledError:
         print('错误')
@@ -43,23 +64,26 @@ def pdf_to_jpeg(pdf, jpeg, index):
 def main(argv):
     input_pdf = ''
     output_jpeg = ''
-    page = ''
+    start = 0
+    end = 0
     try:
-        opts, args = getopt.getopt(argv, "i:o:p:", ["input=", "output=", "page="])
+        opts, args = getopt.getopt(argv, "i:o:s:e:", ["input=", "output=", "start=", "end="])
         for opt, arg in opts:
             if opt in ("-i", "--input"):
                 input_pdf = arg
             elif opt in ("-o", "--output"):
                 output_jpeg = arg
-            elif opt in ("-p", "--page"):
-                page = arg
-        pdf_to_jpeg(input_pdf, output_jpeg, int(page) - 1)
+            elif opt in ("-s", "--start"):
+                start = int(arg)
+            elif opt in ("-e", "--end"):
+                end = int(arg)
+        pdf_to_jpeg(input_pdf, output_jpeg, start, end)
     except getopt.GetoptError:
-        print('用法：pdf转换.exe -i <input> -o <output> -p <page>')
+        print('用法：pdf转换.exe -i <input> -o <output> -s <start page> -e <end page>')
         sys.exit(2)
     except Exception as err:
         print(str(err))
-        print('用法：pdf转换.exe -i <input pdf> -o <output jpeg> -p <page>')
+        print('用法：pdf转换.exe -i <input> -o <output> -s <start page> -e <end page>')
         sys.exit(2)
 
 
